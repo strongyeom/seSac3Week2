8d9e48260864da70bd2c84c5ec36d34d396a3142
yeomsungpil <present7256@gmail.com>
2023-07-24 11:11:15 +0900

    커밋 시작

--- seSac3Week2/ViewController.swift
+++ seSac3Week2/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("imageView",imageView.isUserInteractionEnabled)
+        print("view",view.isUserInteractionEnabled)
     }
 
-
+    @IBAction func tapGestureClicked(_ sender: UITapGestureRecognizer) {
+        print("image")
+    }
+    
+    @IBAction func testtapGestureClicked(_ sender: UITapGestureRecognizer) {
+        print("empty view")
+    }
+    
+    
 }
 
