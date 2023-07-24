a54421649abd3225113a84ca7d6c37952685952b
yeomsungpil <present7256@gmail.com>
2023-07-24 14:47:44 +0900

    unWind 를 통해 돌아가기 버튼 만듬 / 장점 : 원하는 화면으로 한번에 돌아 갈 수있음

--- seSac3Week2/TransitionViewController.swift
+++ seSac3Week2/TransitionViewController.swift
@@ -21,7 +21,12 @@ class TransitionViewController: UIViewController {
       
     }
    
-    
+    // 다른 화면에서 TransitionVC로 돌아올 때 (unWind) 사용하는 기능
+    // 액션 연결 하지 않고, 직접 작성
+    // 백버튼 메서드
+    @IBAction func unWindButton(_ segue: UIStoryboardSegue) {
+        
+    }
     
     
     
