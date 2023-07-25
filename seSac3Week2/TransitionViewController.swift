ae2d55871ebd5fea30ee3c56eb9305643d5d76be
yeomsungpil <present7256@gmail.com>
2023-07-25 11:31:51 +0900

    kingfisher를 사용해서 이미지 불러오기

--- seSac3Week2/TransitionViewController.swift
+++ seSac3Week2/TransitionViewController.swift
@@ -15,15 +15,11 @@ class TransitionViewController: UIViewController {
     
     @IBOutlet var imageView: UIImageView!
     
-
+    var webImage: String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
-        let url = URL(string: "https://images.unsplash.com/photo-1690148136337-2304c30b5420?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80")
-        imageView.kf.setImage(with: url)
-        
+        imageView.image = UIImage(systemName: "photo")
       
     }
    
@@ -33,6 +29,27 @@ class TransitionViewController: UIViewController {
     @IBAction func unWindButton(_ segue: UIStoryboardSegue) {
         
     }
+    
+    @IBAction func segmentClicked(_ sender: UISegmentedControl) {
+        print("sender \(sender.selectedSegmentIndex)")
+        
+        if sender.selectedSegmentIndex == 0 {
+            
+            webImage = "https://images.unsplash.com/photo-1690148136337-2304c30b5420?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80"
+            
+            
+            
+        } else if sender.selectedSegmentIndex == 1 {
+            webImage = "https://images.unsplash.com/photo-1690011510598-320544ba4f1a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDEyfDZzTVZqVExTa2VRfHxlbnwwfHx8fHw%3D&auto=format&fit=crop&w=800&q=60"
+        } else {
+            
+        }
+        
+        let url = URL(string: webImage)
+        imageView.kf.setImage(with: url)
+        
+    }
+    
     
     
     
